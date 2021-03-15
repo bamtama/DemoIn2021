a441b73067c460ca271eca55c45653c02057be0f
bamtama <31029752+bamtama@users.noreply.github.com>
2021-03-15 12:48:07 +0800

    sites remove

--- sites/06/js/chunk-2d0a4838.4ec1cc26.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0a4838"],{"0783":function(n,e,t){"use strict";t.r(e);var l=function(){var n=this,e=n.$createElement,t=n._self._c||e;return t("div",[n._v("log")])},u=[],c={},a=c,o=t("2877"),r=Object(o["a"])(a,l,u,!1,null,null,null);e["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0a4838.4ec1cc26.js.map
