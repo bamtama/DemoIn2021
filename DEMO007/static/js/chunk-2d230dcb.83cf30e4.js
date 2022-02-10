69bbddda4fa13967475125b79cf6b79d39918238
bamtama <31029752+bamtama@users.noreply.github.com>
2022-02-10 17:30:47 +0800

    ADD A NEW DEMO

---
+++ DEMO007/static/js/chunk-2d230dcb.83cf30e4.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d230dcb"],{eda7:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("router-view")],1)},c=[],o={name:"Books"},r=o,a=t("2877"),l=Object(a["a"])(r,u,c,!1,null,null,null);n["default"]=l.exports}}]);
